9613d0d04e0694a1803bfeab545b3d93b9ac8dfd
Daler Muminov <muminovdaler@gmail.com>
2018-08-01 12:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
-console.log(specialGuest)
+console.log(specialGuest);
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}`;
+console.log(conversation);
